bd95431f1f166b9524dd1090edbca6644ba161d3
wenyu Xu <115910849+Xwyturbo@users.noreply.github.com>
2025-02-11 15:28:26 +0800

    Update and rename 1.shp批量重命名.py to 04.shp批量重命名及合并.py

--- 04.shp批量重命名及合并.py
+++ 04.shp批量重命名及合并.py
@@ -18,16 +18,4 @@ for fc in fcs:
     lspt.append(fc)
 arcpy.Merge_management(lspt,"缓冲区区合并.shp")
 
-import sys
-reload(sys)
-sys.setdefaultencoding( "utf-8" )
-import arcpy
-import string
-arcpy.env.workspace  = r"C:\Users\Administrator\Desktop\s\pro\A49.gdb"
-out = r"C:\Users\Administrator\Desktop\s\data100\da.gdb"
-na = arcpy.env.workspace.split("\\")[-1].split(".")[0]
-Files = arcpy.ListFeatureClasses()
-for File in Files:
-    if File == "BOUA":
-        arcpy.Copy_management(File, out+"\\"+na)
 
